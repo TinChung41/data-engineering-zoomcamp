ea65d3ee495969c6fa28ab8cc29a8135de241aef
TinChung41 <tinchung21@gmail.com>
2024-03-02 14:24:10 +0700

    fix the format to download bulk data

--- 05-batch/code/download_data.sh
+++ 05-batch/code/download_data.sh
@@ -1,17 +1,20 @@
-
 set -e
 
 TAXI_TYPE=$1 # "yellow"
 YEAR=$2 # 2020
-# https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
-# https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2021-01.parquet
 
+# https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz
+# https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-02.csv.gz
+# https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-01.csv.gz
+# URL_PREFIX=" https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green"
 URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+
 
 for MONTH in {1..12}; do
   FMONTH=`printf "%02d" ${MONTH}`
 
   URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+  echo ${URL}
 
   LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
   LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
@@ -19,6 +22,6 @@ for MONTH in {1..12}; do
 
   echo "downloading ${URL} to ${LOCAL_PATH}"
   mkdir -p ${LOCAL_PREFIX}
-  curl -O ${URL} -O ${LOCAL_PATH}
+  curl -L ${URL} -o ${LOCAL_PATH}
 
 done
